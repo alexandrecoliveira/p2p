48f6b4f3373194bb6382ca08eb9151b4f19ea906
Alexandre Costa <alecosta19@gmail.com>
2020-03-01 15:31:34 -0300

    Adição do laço do-While que testa se o operador é válido em tx.py

--- tx.py
+++ tx.py
@@ -2,15 +2,18 @@
 import serial
 ser = serial.Serial('/dev/pts/3')
 print(ser.portstr)
-print "'*** CALCULADORA ***"
+print "'*** CALCULADORA ***\n"
 n1 = raw_input("Digite o primeiro número: ")
 while True:
 	n2 = int(raw_input("Digite o segundo número: "))
 	if(n2 != 0):
 		break
-oper = raw_input("Digite o operador (+, -, *, /): ")
+while True:
+	oper = raw_input("Digite o operador (+, -, *, /): ")
+	if(oper == '+' or oper == '-' or oper == '*' or oper == '/'):
+		break
 ser.write(n1 + ";" + str(n2) + ";" + oper + ";" + "\n")
-print "Dados Enviados"
+print "Dados Enviados\n"
 cli = ser.readline()
 print "Reposta: " + cli
 ser.close()
