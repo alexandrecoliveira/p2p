42df5354fdddfb10ec6f660027e9dab83b2583fa
Alexandre Costa <alecosta19@gmail.com>
2020-03-01 15:21:25 -0300

    Laço do-While que testa se o n2 é diferente de 0 em tx.py

--- tx.py
+++ tx.py
@@ -4,9 +4,12 @@ ser = serial.Serial('/dev/pts/3')
 print(ser.portstr)
 print "'*** CALCULADORA ***"
 n1 = raw_input("Digite o primeiro número: ")
-n2 = raw_input("Digite o segundo número: ")
+while True:
+	n2 = int(raw_input("Digite o segundo número: "))
+	if(n2 != 0):
+		break
 oper = raw_input("Digite o operador (+, -, *, /): ")
-ser.write(n1 + ";" + n2 + ";" + oper + ";" + "\n")
+ser.write(n1 + ";" + str(n2) + ";" + oper + ";" + "\n")
 print "Dados Enviados"
 cli = ser.readline()
 print "Reposta: " + cli
