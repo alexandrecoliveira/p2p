30bec72a67635c7c75d1c1f0816df61e43937d6f
Alexandre Costa <alecosta19@gmail.com>
2020-03-01 15:10:32 -0300

    Adiçao do contole de fluxo que realiza a operação matemática solicitada em rx.py

--- rx.py
+++ rx.py
@@ -4,6 +4,16 @@ ser = serial.Serial('/dev/pts/4')
 print(ser.portstr)
 r = ser.readline()
 dados = r.split(";")
-print dados
+if(dados[2] == '+'):
+	r = int(dados[0]) + int(dados[1]) 
+elif(dados[2] == '-'):
+	r = int(dados[0]) - int(dados[1]) 
+elif(dados[2] == '*'):
+	r = int(dados[0]) * int(dados[1])
+else:
+	r = int(dados[0]) / int(dados[1])
+
+print r
+ser.write(str(r) + '\n')
 ser.write("FIM\n")
 ser.close()
